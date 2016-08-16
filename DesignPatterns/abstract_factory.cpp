4a5cd8c1231b33d43d93bf6c3835c191003c7e7d
Neo He <neohe2007@gmail.com>
2016-08-16 12:51:05 +0800

    define abstract factory class

--- DesignPatterns/abstract_factory.cpp
+++ DesignPatterns/abstract_factory.cpp
@@ -7,3 +7,58 @@
 //
 
 #include "abstract_factory.hpp"
+#include "product_a.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+AbstractFactory::AbstractFactory()
+{
+    
+}
+
+AbstractFactory::~AbstractFactory()
+{
+    
+}
+
+ConcreteFactory1::ConcreteFactory1()
+{
+    
+}
+
+ConcreteFactory1::~ConcreteFactory1()
+{
+    
+}
+
+AbstractProductA *ConcreteFactory1::CreateProductA()
+{
+    return new ProductA1();
+}
+
+AbstractProductB *ConcreteFactory1::CreateProductB()
+{
+    return new ProductB1();
+}
+
+ConcreteFactory2::ConcreteFactory2()
+{
+    
+}
+
+ConcreteFactory2::~ConcreteFactory2()
+{
+    
+}
+
+AbstractProductA *ConcreteFactory2::CreateProductA()
+{
+    return new ProductA2();
+}
+
+AbstractProductB *ConcreteFactory2::CreateProductB()
+{
+    return new ProductB2();
+}
