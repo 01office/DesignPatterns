479488f2e150ac9c22b6f925c54e0e61ab40fa94
Neo He <neohe2007@gmail.com>
2016-09-27 00:32:44 +0800

    design pattern: observer

---
+++ DesignPatterns/observer.cpp
@@ -0,0 +1,9 @@
+//
+//  observer.cpp
+//  DesignPatterns
+//
+//  Created by heyong on 16/9/27.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include "observer.hpp"
---
+++ DesignPatterns/observer.hpp
@@ -0,0 +1,14 @@
+//
+//  observer.hpp
+//  DesignPatterns
+//
+//  Created by heyong on 16/9/27.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#ifndef observer_hpp
+#define observer_hpp
+
+#include <stdio.h>
+
+#endif /* observer_hpp */
