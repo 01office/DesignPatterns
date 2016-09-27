3de4fe4d2f6c02ab5eeb1ae97d5dacab3da5b974
Neo He <neohe2007@gmail.com>
2016-09-27 08:46:05 +0800

    test observer

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -16,12 +16,14 @@
 
 #include "strategy.hpp"
 
+#include "observer.hpp"
+
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    
+/*
     Factory *fac = new ConcreteFactory();
     fac->CreateProduct();
     
@@ -43,6 +45,24 @@ int main(int argc, const char * argv[]) {
     Strategy *psb = new ConcreteStrategyB();
     Contxt *pcb = new Contxt(psb);
     pcb->doAction();
+*/
+    
+    Observer *O1 = new ConcreteObserverA();
+    Observer *O2 = new ConcreteObserverB();
+    
+    Subject *Ps1 = new ConcreteSubjectA();
+    Ps1->Attach(O1);
+    Ps1->Attach(O2);
+    
+    Ps1->SetState("old");
+    
+    Ps1->Notify();
+    
+    Ps1->Detach(O1);
+    
+    Ps1->SetState("new");
+    
+    Ps1->Notify();
     
     std::cout << "Hello, World!\n";
     return 0;
