2abae86763afff5dd07873f34417347bff7b80e5
Neo He <neohe2007@gmail.com>
2016-10-15 23:19:40 +0800

    design pattern: simple factory

---
+++ DesignPatterns/SimpleFactory.cpp
@@ -0,0 +1,9 @@
+//
+//  SimpleFactory.cpp
+//  DesignPatterns
+//
+//  Created by heyong on 2016/10/15.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include "SimpleFactory.hpp"
---
+++ DesignPatterns/SimpleFactory.hpp
@@ -0,0 +1,14 @@
+//
+//  SimpleFactory.hpp
+//  DesignPatterns
+//
+//  Created by heyong on 2016/10/15.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#ifndef SimpleFactory_hpp
+#define SimpleFactory_hpp
+
+#include <stdio.h>
+
+#endif /* SimpleFactory_hpp */
