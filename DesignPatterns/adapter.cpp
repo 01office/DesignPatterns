75f995e7c74099f5d656f1f908a0d5773dfc501d
Neo He <neohe2007@gmail.com>
2016-10-22 00:02:24 +0800

    init adapter pattern

---
+++ DesignPatterns/adapter.cpp
@@ -0,0 +1,9 @@
+//
+//  adapter.cpp
+//  DesignPatterns
+//
+//  Created by heyong on 2016/10/22.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#include "adapter.hpp"
---
+++ DesignPatterns/adapter.hpp
@@ -0,0 +1,14 @@
+//
+//  adapter.hpp
+//  DesignPatterns
+//
+//  Created by heyong on 2016/10/22.
+//  Copyright © 2016年 heyong. All rights reserved.
+//
+
+#ifndef adapter_hpp
+#define adapter_hpp
+
+#include <stdio.h>
+
+#endif /* adapter_hpp */
